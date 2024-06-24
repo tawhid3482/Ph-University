75555ec755057df8cf0e2918f2103891de431991
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-24 10:28:13 +0600

    server working

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
