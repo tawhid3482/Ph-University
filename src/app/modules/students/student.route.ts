7bea8494a2ab40140a9e3aab2ef350a440256bc3
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-29 09:27:27 +0600

    student service

--- src/app/modules/students/student.route.ts
+++ src/app/modules/students/student.route.ts
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.post('/create-student', studentControllers.createStudent);
 router.get('/', studentControllers.getAllStudent);
+router.get('/:studentId', studentControllers.getSingleStudent);
 
 export const StudentRoutes = router;
--- src/app/modules/students/student.service.ts
+++ src/app/modules/students/student.service.ts
@@ -11,8 +11,13 @@ const createStudentIntoDB = async (student: Student) => {
     const result = await StudentModel.find();
     return result
   }
+  const getSingleStudentFromDB = async (id: string) => {
+    const result = await StudentModel.findOne({ id });
+    return result;
+  };
 
 export const StudentServices = {
     createStudentIntoDB,
-    getAllStudentFromDB
+    getAllStudentFromDB,
+    getSingleStudentFromDB
 }
