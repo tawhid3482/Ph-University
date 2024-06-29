743e4d7f37f3da62912ac6baf87a922148a4d373
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-29 09:28:00 +0600

    student controller

--- src/app/modules/students/student.controller.ts
+++ src/app/modules/students/student.controller.ts
@@ -30,8 +30,24 @@ const createStudent = async (req: Request, res: Response) => {
         console.log(error)
     }
   }
+  const getSingleStudent = async (req: Request, res: Response) => {
+    try {
+      const { studentId } = req.params;
+  
+      const result = await StudentServices.getSingleStudentFromDB(studentId);
+  
+      res.status(200).json({
+        success: true,
+        message: 'Student is retrieved succesfully',
+        data: result,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
 export const studentControllers = {
     createStudent,
-    getAllStudent
+    getAllStudent,
+    getSingleStudent
 }
