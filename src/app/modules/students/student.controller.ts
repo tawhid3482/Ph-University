2b1aa660d3799895e22f3c587b7b4b16001b0d57
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-03 09:17:38 +0600

    controller.ts

--- src/app/modules/students/student.controller.ts
+++ src/app/modules/students/student.controller.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from "express";
 import { StudentServices } from "./student.service";
+import studentValidationSchema from "./student.validation";
 
 const createStudent = async (req: Request, res: Response) => {
     try {
       const { student: studentData } = req.body;
+      const {error} = studentValidationSchema.validate(studentData)
       const result = await StudentServices.createStudentIntoDB(studentData);
   
+      if(error){
+        res.status(500).json({
+          success: false,
+          message: 'Student is not created ',
+          error: error.details,
+        });
+      }
+
       res.status(200).json({
         success: true,
         message: 'Student is created succesfully',
