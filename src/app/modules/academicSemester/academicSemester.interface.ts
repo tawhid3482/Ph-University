9f6174b6b43e54e4275b627e440e462b886506a9
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-09 20:12:54 +0600

    Academic Semester interface

---
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -0,0 +1,10 @@
+export type TMonth = 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December';
+
+
+ export type TAcademicSemester ={
+    name: 'Autumn'| 'summar'| 'Fall',
+    code: '01'| '02'| '03',
+    year: Date,
+    startMonth : TMonth,
+    endMonth: TMonth,
+}
