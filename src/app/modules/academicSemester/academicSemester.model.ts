31960f1aa96c740c8d708e836ca7def6bfdba11b
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-10 10:23:35 +0600

    Academic Semester model

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,23 +1,48 @@
-import { Schema } from 'mongoose';
+import { model, Schema } from 'mongoose';
 import { TAcademicSemester, TMonth } from './academicSemester.interface';
 
-
-
-const academicSemesterSchema = new Schema<TAcademicSemester>({
-  name: {
-    type: String,
-    enum: {
-      values: ['Autumn', 'Summar', 'Fall'],
-      message: 'Name is required',
+const MonthSchema: TMonth[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const academicSemesterSchema = new Schema<TAcademicSemester>(
+  {
+    name: {
+      type: String,
+      enum: {
+        values: ['Autumn', 'Summar', 'Fall'],
+        message: 'Name is required',
+      },
+      required: true,
     },
-  },
-  code: {
-    type: String,
-    enum: {
-      values: ['01', '02', '03'],
-      message: 'Code is required',
+    code: {
+      type: String,
+      enum: {
+        values: ['01', '02', '03'],
+        message: 'Code is required',
+      },
+      required: true,
     },
+    year: { type: Date, required: true },
+    startMonth: { type: String, enum: MonthSchema },
+    endMonth: { type: String, enum: MonthSchema },
   },
-  year: { type: Date },
-  startMonth:
-});
+  {
+    timestamps: true,
+  }
+);
+
+export const academicSemesterModel = model<TAcademicSemester>(
+  'academicSemester',
+  academicSemesterSchema
+);
---
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -0,0 +1,7 @@
+
+import {z}from 'zod';
+
+export const academicSemesterValidationSchema = z.object({
+    name: z.string(),
+    code: 
+})
