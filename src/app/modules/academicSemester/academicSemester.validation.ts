5e5a4d2bf153edc5710d6ee52ae10fc79a2361e2
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-12 19:15:16 +0600

    academic semester validation

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -1,12 +1,13 @@
-import { z } from 'zod';
+import { string, z } from 'zod';
+import { MonthSchema, semesterCodeSchema, semesterNameSchema } from './academicSemester.constant';
 
 export const academicSemesterValidationSchema = z.object({
   body: z.object({
-    name: z.string(),
-    code: z.string(),
-    year: z.string(),
-    startMonth: z.string(),
-    endMonth: z.string(),
+    name: z.enum([...semesterNameSchema] as [string, ...string[]]),
+    code: z.enum([...semesterCodeSchema] as [string, ...string[]]),
+    year: z.date(),
+    startMonth: z.enum([...MonthSchema] as [string, ...string[]]),
+    endMonth: z.enum([...MonthSchema] as [string, ...string[]]),
   }),
 });
 
