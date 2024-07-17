a644c605f62447406735b1f0a175a329900a970a
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-17 09:53:43 +0600

    validation

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -3,7 +3,7 @@ import { TAcademicDepartment } from './academicDepartment.interface';
 
 const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   {
-    name: { type: String, unique:true, required: true },
+    name: { type: String, unique: true, required: true },
     academicFaculty: {
       type: Schema.Types.ObjectId,
       ref: 'AcademicFaculty',
@@ -14,6 +14,22 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
     timestamps: true,
   }
 );
+
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await academicDepartmentModel.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExist) {
+    throw new Error('This Department already exist');
+  }
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function(next){
+  
+next()
+})
+
 
 export const academicDepartmentModel = model<TAcademicDepartment>(
   'AcademicDepartment',
