b1129bd7ce0639607506b194d39fa477783840b2
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-18 10:59:19 +0600

    globalError

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
 
 const globalErrorHandler = (
   err: any,
@@ -11,8 +13,7 @@ const globalErrorHandler = (
   let statusCode = err.statusCode || 500;
   let message = err.message || 'Something went wrong';
 
-
-  const errorSources: TErrorSource = [
+  let errorSources: TErrorSource = [
     {
       path: '',
       message: 'Something went wrong',
@@ -20,7 +21,7 @@ const globalErrorHandler = (
   ];
 
   const handleZodError = (err: ZodError) => {
-    const errorSources :TErrorSource = err.issues.map((issue: ZodIssue) => {
+    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
       return {
         path: issue?.path[issue.path.length - 1],
         message: issue.message,
@@ -37,12 +38,16 @@ const globalErrorHandler = (
 
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
 
   return res.status(statusCode).json({
     success: false,
     message,
     errorSources,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 export default globalErrorHandler;
