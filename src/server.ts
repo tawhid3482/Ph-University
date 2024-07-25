2342618abb10596e530297b8ea7c38555343d8e5
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-25 09:19:36 +0600

    server shut down

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
+import { Server } from 'http';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+   server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
@@ -14,3 +17,20 @@ async function main() {
   }
 }
 main();
+
+// unhandled rejection thake server close korar jnno
+process.on('unhandledRejection', ()=>{
+  console.log(`shut down...`)
+  if(server){
+    server.close(()=>{
+      process.exit(1)
+    })
+  }
+  process.exit(1)
+})
+
+process.on('uncaughtException',()=>{
+  console.log(`uncaughtException shut down...`)
+
+  process.exit(1)
+})
