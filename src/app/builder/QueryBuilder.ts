ae80ab3254e045659a4e5a75c0373fb565a15a97
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-26 10:23:23 +0600

    searching, filtering

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -1,14 +1,35 @@
-import { Query } from "mongoose";
+import { FilterQuery, Query } from 'mongoose';
 
 class QueryBuilder<T> {
-    public modelQuery: Query<T[],T>;
-    public query: Record<string,unknown>;
-    constructor (modelQuery: Query<T[],T>,  query: Record<string,unknown>){
-        this.modelQuery = modelQuery
-        this.query = query
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+
+  search(searchableField: string[]) {
+    if (this?.query?.searchTerm) {
+      this.modelQuery = this.modelQuery.find({
+        $or: searchableField.map(
+          (field) =>
+            ({
+              [field]: { $regex: searchTerm, $options: 'i' },
+            } as FilterQuery<T>)
+        ),
+      });
     }
+    return this;
+  }
 
-     
-    
+  filter() {
+    const queryObj = { ...this.query };
 
+    // filtering
+    const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
+    return this;
+  }
 }
