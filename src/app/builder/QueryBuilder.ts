b6b7d4235167383dacbfd7c22eaeaefce1d3d7cf
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-26 10:10:26 +0600

    querybuilder

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -1,3 +1,14 @@
-class QueryBuilder {
+import { Query } from "mongoose";
+
+class QueryBuilder<T> {
+    public modelQuery: Query<T[],T>;
+    public query: Record<string,unknown>;
+    constructor (modelQuery: Query<T[],T>,  query: Record<string,unknown>){
+        this.modelQuery = modelQuery
+        this.query = query
+    }
+
+     
     
+
 }
