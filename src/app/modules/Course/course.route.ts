44215a6bd15aa41791f3ca8a008a50450af6c212
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-29 19:37:32 +0600

    course route

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -1,1 +1,15 @@
 import express from 'express'
+import validationRequest from '../../middleware/validateRequest'
+import { CourseValidation } from './course.validation'
+import { CourseController } from './course.controller'
+
+
+
+const route = express.Router()
+
+
+route.post('create-course', validationRequest(CourseValidation.courseValidationSchema), CourseController.createCourse)
+route.get('/',CourseController.getAllCourse)
+route.get('/:id',CourseController.getSingleCourse)
+route.delete('/:id',CourseController.deleteCourse)
+route.patch('/:id',CourseController.updateCourse)
