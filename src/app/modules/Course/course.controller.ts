4afb759e6770b371849dcbdb6b89b31350d4a679
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-29 19:25:50 +0600

    course controller

---
+++ src/app/modules/Course/course.controller.ts
@@ -0,0 +1,68 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { courseServices } from "./course.service";
+
+
+const createCourse = catchAsync(async(req,res)=>{
+    const result  = await courseServices.createCourseIntoDB(req.body)
+
+    sendResponse(res,{
+        statusCode:httpStatus.CREATED,
+        success:true,
+        message:'Course Created Successfully',
+        data:result
+    })
+})
+const getAllCourse = catchAsync(async(req,res)=>{
+    const result  = await courseServices.getAllCourseFromDB()
+
+    sendResponse(res,{
+        statusCode:httpStatus.CREATED,
+        success:true,
+        message:'Course retrieved Successfully',
+        data:result
+    })
+})
+const getSingleCourse = catchAsync(async(req,res)=>{
+    const {id}= req.params
+    const result  = await courseServices.getSingleCourseFromDB(id)
+
+    sendResponse(res,{
+        statusCode:httpStatus.CREATED,
+        success:true,
+        message:'Course retrieved Successfully',
+        data:result
+    })
+})
+const deleteCourse = catchAsync(async(req,res)=>{
+    const {id}= req.params
+    const result  = await courseServices.deleteCourseFromDB(id)
+
+    sendResponse(res,{
+        statusCode:httpStatus.CREATED,
+        success:true,
+        message:'Course retrieved Successfully',
+        data:result
+    })
+})
+const updateCourse = catchAsync(async(req,res)=>{
+    const {id}= req.params
+    const result  = await courseServices.updateCourseFromDB(id,req.body)
+
+    sendResponse(res,{
+        statusCode:httpStatus.CREATED,
+        success:true,
+        message:'Course updated Successfully',
+        data:result
+    })
+})
+
+
+export const CourseController = {
+    createCourse,
+    getAllCourse,
+    deleteCourse,
+    getSingleCourse,
+    updateCourse
+}
