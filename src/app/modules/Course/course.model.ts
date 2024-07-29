6fbd374f85e19c2b0f8b60dfd791fe832728f1e9
tawhidul islam <tawhidulislam3482@gmail.com>
2024-07-29 15:37:36 +0600

    course model

--- src/app/modules/Course/course.model.ts
+++ src/app/modules/Course/course.model.ts
@@ -1,9 +1,17 @@
-import { Schema } from "mongoose";
-import { TCourse } from "./course.interface";
+import { model, Schema, Types } from 'mongoose';
+import { TCourse, TPreRequisiteCourses } from './course.interface';
+
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
+  course: { type: Schema.Types.ObjectId },
+  isDeleted: {type: Boolean, default: false} 
+});
 
 const courseSchema = new Schema<TCourse>({
-    title:{type:String, required:true},
-    prefix:{type:String, required:true},
-    code:{type:Number, required:true},
-    credits:{type:Number, required:true},
-})
+  title: { type: String, unique: true, trim: true, required: true },
+  prefix: { type: String, trim: true, required: true },
+  code: { type: Number, required: true },
+  credits: { type: Number, required: true },
+  preRequisiteCourses: {},
+});
+
+export const courseModel = model<TCourse>('Course', courseSchema)
