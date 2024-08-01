13de9196172bf09b2c9c96075695ee491f73923b
tawhidul islam <tawhidulislam3482@gmail.com>
2024-08-01 10:21:26 +0600

    semesterRegistration servic

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -15,3 +15,7 @@ const createSemesterRegistration = catchAsync(async (req, res) => {
     data: result,
   });
 });
+
+export const semesterRegistrationControllers = {
+    createSemesterRegistration,
+}
--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import validationRequest from '../../middleware/validateRequest';
 import { semesterRegistrationValidation } from './semesterRegistration.validation';
+import { semesterRegistrationControllers } from './semesterRegistration.controller';
 
 const route = express.Router()
 
 route.post('/create-semesterRegistration', validationRequest(semesterRegistrationValidation.semesterRegistrationValidationSchema), semesterRegistrationControllers.createSemesterRegistration)
 
-route.get('/', semesterRegistrationControllers.getAllSemesterRegistration)
+// route.get('/', semesterRegistrationControllers.getAllSemesterRegistration)
 
 
 export const semesterRegistrationRoutes = route
