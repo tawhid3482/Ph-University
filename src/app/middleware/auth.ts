7e0feeaad9843d35b34eba099b0ac08685410a85
tawhidul islam <tawhidulislam3482@gmail.com>
2024-08-17 09:14:23 +0600

    token check

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import catchAsync from '../utils/catchAsync';
+import AppError from '../errors/AppError';
+import httpStatus from 'http-status';
+
+const auth = () => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // auth token validation check
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
+    next();
+  });
+};
+
+export default auth;
