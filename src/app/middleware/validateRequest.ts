2fa6f3738c23fc67bcebfc6e0d7450ecbe98aec3
tawhidul islam <tawhidulislam3482@gmail.com>
2024-08-17 09:06:02 +0600

    Auth JWt

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -1,18 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject } from 'zod';
+import catchAsync from '../utils/catchAsync';
 
 const validationRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // validation check
-      await schema.parseAsync({
-        body: req.body,
-      });
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // validation check
+    await schema.parseAsync({
+      body: req.body,
+    });
+    next();
+  });
 };
 
 export default validationRequest;
